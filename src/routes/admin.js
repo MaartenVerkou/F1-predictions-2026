8f123762a5d4c78a5460b7ac4912d498bfa0be84
Maarten Verkou <maartenverkou@hotmail.com>
2026-09-22 07:47:57 +0000

    fix: make non-active actuals read-only

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3260,6 +3260,7 @@ function registerAdminRoutes(app, deps) {
       selectedSnapshotMeta,
       pendingReviewTargets,
       requiresPastUnlock,
+      allowPastEdit,
       isFutureRaceTarget,
       hasDraft: Boolean(draftActuals),
       saveError,
